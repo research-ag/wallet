391e98d4c7d47ed93b85863d409e82558c1757a4
JairSDS <jair@sdsharp.com>
2023-12-05 16:20:01 -0500

    auto selection subaccount from selected asset

--- frontend/pages/home/hooks/assetHook.tsx
+++ frontend/pages/home/hooks/assetHook.tsx
@@ -37,6 +37,47 @@ export const AssetHook = () => {
   const [newSub, setNewSub] = useState<SubAccount | undefined>();
   const [hexChecked, setHexChecked] = useState<boolean>(false);
 
+<<<<<<< HEAD
+=======
+  const reloadBallance = (tkns?: Token[]) => {
+    dispatch(setLoading(true));
+    updateAllBalances(true, userAgent, tkns ? tkns : tokens.length > 0 ? tokens : defaultTokens);
+  };
+
+  const getTotalAmountInCurrency = () => {
+    let amount = 0;
+    assets.map((tk) => {
+      const market = tokensMarket.find((tm) => tm.symbol === tk.tokenSymbol);
+      let assetTotal = 0;
+      tk.subAccounts.map((sa) => {
+        assetTotal = assetTotal + Number(sa.amount);
+      });
+      amount = amount + (market ? assetTotal * market.price : 0);
+    });
+    return Math.round(amount * 100) / 100;
+  };
+
+  useEffect(() => {
+    const auxAssets = assets.filter((asset) => {
+      let includeInSub = false;
+      asset.subAccounts.map((sa) => {
+        if (sa.name.toLowerCase().includes(searchKey.toLowerCase())) includeInSub = true;
+      });
+
+      return asset.name.toLowerCase().includes(searchKey.toLowerCase()) || includeInSub || searchKey === "";
+    });
+
+    if (auxAssets.length > 0) {
+      const isSelected = auxAssets.find((ast) => ast.tokenSymbol === selectedAsset?.tokenSymbol);
+      if (selectedAsset && !isSelected) {
+        setAcordeonIdx(`asset-${auxAssets[0].sort_index}`);
+        dispatch(setSelectedAsset(auxAssets[0]));
+        auxAssets[0].subAccounts.length > 0 && dispatch(setSelectedAccount(auxAssets[0].subAccounts[0]));
+      }
+    }
+  }, [searchKey]);
+
+>>>>>>> 0a5fdf8e (auto selection subaccount from selected asset)
   return {
 <<<<<<< HEAD
 =======
