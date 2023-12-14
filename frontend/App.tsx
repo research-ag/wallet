ff02466dce248377eea88d66cd42a81bee13cda6
Julio DEV <julio.f@sdsharp.com>
2023-12-14 11:28:11 -0400

    feat: disclamer modal isolation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -39,7 +39,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <Provider store={store}>
-        <SwitchRoute></SwitchRoute>
+        <SwitchRoute />
       </Provider>
     </div>
   );
