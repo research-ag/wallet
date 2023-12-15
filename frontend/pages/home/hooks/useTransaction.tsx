d4644c5df8789de1c16883eb40f9931f2fcdc06e
JairSDS <jair@sdsharp.com>
2023-12-15 13:08:57 -0500

    load saved tx when change subacc

--- frontend/pages/home/hooks/useTransaction.tsx
+++ frontend/pages/home/hooks/useTransaction.tsx
@@ -2,14 +2,16 @@ import { useAppDispatch, useAppSelector } from "@redux/Store";
 import { useEffect } from "react";
 import { getAllTransactionsICRC1, getAllTransactionsICP } from "@redux/assets/AssetActions";
 import { Transaction } from "@redux/models/AccountModels";
-import { setSelectedTransaction } from "@redux/assets/AssetReducer";
+import { setSelectedTransaction, setTransactions } from "@redux/assets/AssetReducer";
 import { AssetSymbolEnum } from "@/const";
 import { hexToUint8Array } from "@/utils";
 import { Token } from "@redux/models/TokenModels";
 export const UseTransaction = () => {
   const dispatch = useAppDispatch();
 
-  const { tokens, selectedAsset, selectedAccount, selectedTransaction } = useAppSelector((state) => state.asset);
+  const { tokens, selectedAsset, selectedAccount, selectedTransaction, txWorker } = useAppSelector(
+    (state) => state.asset,
+  );
 
   const changeSelectedTransaction = (value: Transaction) => dispatch(setSelectedTransaction(value));
 
@@ -36,21 +38,29 @@ export const UseTransaction = () => {
   };
 
   useEffect(() => {
-    if (
-      selectedAsset?.tokenSymbol === AssetSymbolEnum.Enum.ICP ||
-      selectedAsset?.tokenSymbol === AssetSymbolEnum.Enum.OGY
-    ) {
-      const getICPTx = async () => {
-        await getSelectedSubaccountICPTx();
-      };
+    if (selectedAsset) {
+      const founded = txWorker.find((tx) => {
+        return selectedAccount?.symbol === tx.tokenSymbol && selectedAccount.sub_account_id === tx.subaccount;
+      });
 
-      getICPTx();
-    } else {
-      const getICRCTx = async () => {
-        await getSelectedSubaccountICRCTx();
-      };
+      if (founded) dispatch(setTransactions(founded.tx));
 
-      getICRCTx();
+      if (
+        selectedAsset?.tokenSymbol === AssetSymbolEnum.Enum.ICP ||
+        selectedAsset?.tokenSymbol === AssetSymbolEnum.Enum.OGY
+      ) {
+        const getICPTx = async () => {
+          await getSelectedSubaccountICPTx();
+        };
+
+        getICPTx();
+      } else {
+        const getICRCTx = async () => {
+          await getSelectedSubaccountICRCTx();
+        };
+
+        getICRCTx();
+      }
     }
   }, [selectedAccount]);
 
