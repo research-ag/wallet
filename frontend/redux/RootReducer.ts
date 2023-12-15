c29066d45d76e7ea833cfb32824b5c42612bcf49
Julio DEV <julio.f@sdsharp.com>
2023-12-15 18:24:52 -0400

    feat: reducer skeletton and local storage util

--- frontend/redux/RootReducer.ts
+++ frontend/redux/RootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import authReducer from "./auth/AuthReducer";
 import AssetReducer from "./assets/AssetReducer";
 import ContactsReducer from "./contacts/ContactsReducer";
+import allowanceSlice from "./allowances/AllowanceReducer";
 
 const appReducer = combineReducers({
   auth: authReducer,
   asset: AssetReducer,
   contacts: ContactsReducer,
+  allowance: allowanceSlice,
 });
 
 const RootReducer = (state: any, action: any) => {
---
+++ frontend/utils/localStorage.ts
@@ -0,0 +1,23 @@
+const LOCAL_STORAGE_PREFIX = "HPL";
+
+export function setInLocalStorage<T>(key: string, value: T): T | null {
+  try {
+    localStorage.setItem(`${LOCAL_STORAGE_PREFIX}-${key}`, JSON.stringify(value));
+    return value;
+  } catch (error) {
+    return null;
+  }
+}
+
+export function getFromLocalStorage<T>(key: string): T | null {
+  const value = localStorage.getItem(`${LOCAL_STORAGE_PREFIX}-${key}`);
+  return value ? JSON.parse(value) : null;
+}
+
+export const removeFromLocalStorage = (key: string): void => {
+  localStorage.removeItem(`${LOCAL_STORAGE_PREFIX}-${key}`);
+};
+
+export const existInLocalStorage = (key: string): boolean => {
+  return getFromLocalStorage(key) !== null;
+};
