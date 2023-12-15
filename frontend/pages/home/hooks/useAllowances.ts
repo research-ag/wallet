2a7b334bf07e8a85e6dfc91d4fb6ba036e236c20
Julio DEV <julio.f@sdsharp.com>
2023-12-15 18:24:52 -0400

    feat: reducer skeletton and local storage util

---
+++ frontend/pages/home/hooks/useAllowances.ts
@@ -0,0 +1,5 @@
+export default function useAllowances() {
+    // TODO: call redux actions here
+    // TODO: manage request life cycle stages
+  return {};
+}
---
+++ frontend/redux/allowances/AllowanceReducer.ts
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const allowanceSlice = createSlice({
+  name: "allowance",
+  initialState: {},
+  reducers: {},
+});
+
+export const {} = allowanceSlice.actions;
+export default allowanceSlice.reducer;
