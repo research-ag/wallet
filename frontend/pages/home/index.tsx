5ada4867134c91c591dd836598a2a629f780a15b
Julio DEV <julio.f@sdsharp.com>
2024-01-02 21:42:08 -0400

    feat: remove wrong calendar

--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -3,12 +3,10 @@ import AssetsList from "./components/asset/AssetsList";
 import DetailList from "./components/DetailList";
 
 import "./style.scss";
-import { CalendarPicker } from "@components/CalendarPicker";
 
 const Home = () => {
   return (
     <Fragment>
-      <CalendarPicker onDateChange={console.log} />
       <div className="flex flex-row w-full h-full">
         <AssetsList />
         <DetailList />
