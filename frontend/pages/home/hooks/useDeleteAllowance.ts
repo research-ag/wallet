dc174d5dd0f67484a7f802a93f6ced4334ae82c9
Julio DEV <julio.f@sdsharp.com>
2024-01-10 11:04:43 -0400

    feat: verify expiration before to remove from ledger

--- frontend/pages/home/hooks/useDeleteAllowance.ts
+++ frontend/pages/home/hooks/useDeleteAllowance.ts
@@ -5,6 +5,7 @@ import { useMutation } from "@tanstack/react-query";
 import { throttle } from "lodash";
 import { useCallback } from "react";
 import { allowanceFullReload } from "../helpers/allowanceCache";
+import dayjs from "dayjs";
 
 export default function useDeleteAllowance() {
   const onSuccess = async () => {
@@ -17,12 +18,24 @@ export default function useDeleteAllowance() {
 
   const mutationFn = useCallback(async (allowance: TAllowance) => {
     try {
-      if (!allowance?.id) return new Error("Invalid allowance");
-      const params = generateApproveAllowance({ ...allowance, amount: "0", expiration: undefined });
-      await ICRCApprove(params, allowance.asset.address);
-      await removeAllowance(allowance?.id);
+      if (!allowance?.id) {
+        throw new Error("Invalid allowance");
+      }
+
+      if (allowance.expiration || allowance.noExpire) {
+        const currentDate = dayjs();
+        const expirationDate = dayjs(allowance.expiration);
+
+        if (currentDate.isBefore(expirationDate) || allowance.noExpire) {
+          const params = generateApproveAllowance({ ...allowance, amount: "0", expiration: undefined });
+          await ICRCApprove(params, allowance.asset.address);
+        }
+      }
+
+      await removeAllowance(allowance.id);
+      return { success: true };
     } catch (error) {
-      console.log(error);
+      console.error(error);
       return { success: false, error };
     }
   }, []);
