34fa2678973fbf1b659eb3a6026ae096ebc47935
Julio DEV <julio.f@sdsharp.com>
2024-02-06 14:32:15 -0400

    fix: light mode tooltip for allowances

--- frontend/pages/contacts/components/AllowanceTooltip.tsx
+++ frontend/pages/contacts/components/AllowanceTooltip.tsx
@@ -14,14 +14,16 @@ export default function AllowanceTooltip(props: AllowanceTooltipProps) {
   const { amount, expiration, tokenSymbol } = props;
 
   return (
-    <div className="grid mr-2 place-items-center">
+    <div className="grid mr-2 place-items-center ">
       <ToolTip trigger={<MoneyHandIcon className="relative w-5 h-5 cursor-pointer fill-RadioCheckColor" />}>
         <div className="h-24 w-fit">
           <h2 className="text-lg opacity-50 dark:text-PrimaryTextColor text-PrimaryTextColorLight">{t("amount")}</h2>
           <h2 className="text-lg dark:text-PrimaryTextColor text-PrimaryTextColorLight">
             {amount} {tokenSymbol}
           </h2>
-          <h2 className="text-lg opacity-50 dark:text-PrimaryTextColor text-PrimaryTextColorLight">{t("expiration")}</h2>
+          <h2 className="text-lg opacity-50 dark:text-PrimaryTextColor text-PrimaryTextColorLight">
+            {t("expiration")}
+          </h2>
           <h2 className="text-lg dark:text-PrimaryTextColor text-PrimaryTextColorLight">
             {expiration ? formatDateTime(expiration) : t("no.expiration")}
           </h2>
