9e0f20a4cb9a1fc92b61b9516f5687c278552a27
Julio DEV <julio.f@sdsharp.com>
2024-02-06 16:08:06 -0400

    fix: loading gif on transfer validations

--- frontend/pages/home/components/ICRC/detail/transaction/SendForm/ConfirmDetail.tsx
+++ frontend/pages/home/components/ICRC/detail/transaction/SendForm/ConfirmDetail.tsx
@@ -47,7 +47,7 @@ export default function ConfirmDetail({ showConfirmationModal }: ConfirmDetailPr
       <TransactionAmount />
       <div className="flex items-center justify-end mt-6">
         <p className="mr-4 text-md text-slate-color-error">{t(getError())}</p>
-        {isLoading && <LoadingLoader className="mr-4" />}
+        {isLoading && <LoadingLoader color="dark:border-secondary-color-1-light border-black-color mr-2" />}
         <Button className="w-1/6 mr-2 font-bold bg-secondary-color-2" onClick={OnBack}>
           {t("back")}
         </Button>
@@ -99,14 +99,13 @@ export default function ConfirmDetail({ showConfirmationModal }: ConfirmDetailPr
       setIsLoadingAction(true);
       const assetAddress = sender.asset.address;
       const decimal = sender.asset.decimal;
-      
+
       const isValid = await validateBalance();
       if (!isValid) {
         setIsLoadingAction(false);
         return;
       }
 
-      
       if (enableSend && !errors?.length) {
         if (assetAddress && decimal && senderSubAccount && receiverPrincipal && receiverSubAccount && amount) {
           setSendingStatusAction(SendingStatusEnum.Values.sending);
