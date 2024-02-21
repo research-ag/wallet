d63d08a9fa66641cb73dcad7439553a206fcaa16
Julio DEV <julio.f@sdsharp.com>
2024-02-21 16:27:30 -0400

    feat: pass phrase validation

--- frontend/pages/login/components/MnemonicInput.tsx
+++ frontend/pages/login/components/MnemonicInput.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as CheckIcon } from "@assets/svg/files/edit-check.svg";
 import { CustomInput } from "@components/Input";
 import { handleMnemonicAuthenticated } from "@redux/CheckAuth";
 import clsx from "clsx";
-import { ChangeEvent, Dispatch, SetStateAction } from "react";
+import { ChangeEvent, Dispatch, SetStateAction, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 
 interface MnemonicInputProps {
@@ -12,59 +12,59 @@ interface MnemonicInputProps {
 
 export default function MnemonicInput({ phrase, setPhrase }: MnemonicInputProps) {
   const { t } = useTranslation();
+
+  const isValid = useMemo(() => isPhraseValid(phrase), [phrase]);
+
   return (
     <CustomInput
       sizeInput={"medium"}
       intent={"secondary"}
-      compOutClass=""
       placeholder={t("login.mnemonic.expected")}
       value={phrase}
       onChange={onPhraseChange}
       autoFocus
       sufix={
         <div className="flex flex-row items-center justify-start gap-2">
-          <CheckIcon
-            onClick={() => {
-              handleMnemonicAuthenticated(phrase);
-            }}
-            className={getCheckIconStyles(phrase)}
-          />
-          <p className="text-sm w-fit min-w-[4rem] text-PrimaryTextColorLight dark:text-PrimaryTextColor">
+          <CheckIcon onClick={onLoginWithPhase} className={getCheckIconStyles(phrase)} />
+          <p className="text-sm w-fit text-PrimaryTextColorLight dark:text-PrimaryTextColor">
             Words {phrase.split(" ").length}
           </p>
         </div>
       }
       onKeyDown={(e) => {
-        if (e.key === "Enter") handleMnemonicAuthenticated(phrase);
+        if (e.key === "Enter") onLoginWithPhase();
       }}
     />
   );
-  function onPhraseChange(e: ChangeEvent<HTMLInputElement>) {
-    const fullPhrase = e.target.value;
 
-    // INFO: remove leading, ending and double spaces
-    const sanitizedPhrase = fullPhrase
-      .replace(/^\s+/, "")
-      .replace(/\s+$/, "")
-      .replace(/\s{2,}/g, " ")
-      .split(" ");
-
-    if (sanitizedPhrase.length < 12 || sanitizedPhrase.length > 24) {
-      // TODO: Set error state and message
-      console.log("phrase is less than 12 or more than 24");
+  function onLoginWithPhase() {
+    const sanitizedPhrase = getSanitizedPhrase(phrase);
+    const fullPhrase = sanitizedPhrase.join(" ");
+    console.log("isValid: ", isValid);
+    if (isValid) {
+      console.log("Login with phrase: ", fullPhrase);
+      return;
+      // handleMnemonicAuthenticated(fullPhrase);
     }
-
-    const hasExcessivelyLongWord = sanitizedPhrase.some((word) => word.length > 100);
-    if (hasExcessivelyLongWord) {
-      console.warn("phrase has too long words");
-      // TODO: set error "Individual words in the phrase cannot exceed 100 characters.");
-    }
-
-    // TODO: check if is it possible to use less 12 and more 24, what is it recommended
-
-    console.log("final phrase: ", sanitizedPhrase.join(" "));
-    setPhrase(sanitizedPhrase.join(" "));
+    console.log("phrase is not valid for login");
   }
+
+  function onPhraseChange(e: ChangeEvent<HTMLInputElement>) {
+    setPhrase(e.target.value);
+  }
+
+  function isPhraseValid(phrase: string) {
+    const sanitizedPhrase = getSanitizedPhrase(phrase);
+    if (!(sanitizedPhrase.length > 0) || phrase.length === 0) return false;
+    return true;
+  }
+}
+
+function getSanitizedPhrase(phrase: string) {
+  return phrase
+    .trim()
+    .replace(/\s{2, }/g, " ")
+    .split(" ");
 }
 
 function getCheckIconStyles(phrase: string) {
