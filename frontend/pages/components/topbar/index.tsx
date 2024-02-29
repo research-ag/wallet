c648338380510326e23567476e0eefccb4ed2cde
Julio DEV <julio.f@sdsharp.com>
2024-02-29 10:48:22 -0400

    feat: top bar pill session info

--- frontend/pages/components/topbar/index.tsx
+++ frontend/pages/components/topbar/index.tsx
@@ -9,6 +9,8 @@ import { ReactComponent as ICRC1LogoDark } from "@/assets/svg/files/logo_ICRC-1-
 import { ReactComponent as SunIcon } from "@/assets/svg/files/sun-icon.svg";
 import { ReactComponent as WalletIcon } from "@/assets/svg/files/wallet-icon.svg";
 import { ReactComponent as RefreshIcon } from "@/assets/svg/files/refresh-ccw.svg";
+import icUrl from "@/assets/img/icp-logo.png";
+import ethUrl from "@assets/svg/files/ethereum-icon.svg";
 //
 import { Fragment, useState } from "react";
 import { useTranslation } from "react-i18next";
@@ -18,7 +20,6 @@ import { LanguageHook } from "@hooks/languageHook";
 import { clsx } from "clsx";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { AccountHook } from "@hooks/accountHook";
-import { shortAddress } from "@/utils";
 import { logout } from "@redux/CheckAuth";
 import Modal from "@components/Modal";
 import ThemeModal from "./themeModal";
@@ -28,6 +29,9 @@ import { AssetHook } from "@pages/home/hooks/assetHook";
 import { useAppSelector } from "@redux/Store";
 import { db } from "@/database/db";
 import DbLocationModal from "./dbLocationModal";
+import { useSiweIdentity } from "@/siwe";
+import { useAccount } from "wagmi";
+import Pill from "./Pill";
 
 const TopBarComponent = ({ isLoginPage }: { isLoginPage: boolean }) => {
   const { t } = useTranslation();
@@ -39,6 +43,8 @@ const TopBarComponent = ({ isLoginPage }: { isLoginPage: boolean }) => {
 
   const [langOpen, setLangOpen] = useState(false);
   const [dbLocationOpen, setDbLocationOpen] = useState(false);
+  const { identity } = useSiweIdentity();
+  const { address } = useAccount();
 
   const langOpts = [
     { abrev: "en", name: "english", flag: <UsaFlagIcon className={flag} /> },
@@ -50,15 +56,16 @@ const TopBarComponent = ({ isLoginPage }: { isLoginPage: boolean }) => {
   return (
     <Fragment>
       <div className="flex flex-row justify-between min-h-[4.5rem] w-full bg-PrimaryColorLight dark:bg-PrimaryColor text-PrimaryTextColorLight dark:text-PrimaryTextColor border-b border-BorderColorFourthLight dark:border-BorderColorFourth">
-        <div className="flex flex-row justify-start items-center pl-9 gap-24 text-md">
+        <div className="flex flex-row items-center justify-start gap-24 pl-9 text-md">
           {theme === ThemesEnum.enum.dark ? (
             <ICRC1LogoDark className="max-w-[7rem] h-auto" />
           ) : (
             <ICRC1Logo className="max-w-[7rem] h-auto" />
           )}
           {!isLoginPage && (
-            <div className="flex flex-row justify-start items-center gap-3">
-              <p className="opacity-50">{shortAddress(authClient, 12, 10)}</p>
+            <div className="flex flex-row items-center justify-start gap-3">
+              {identity && <Pill text={address as string} start={6} end={4} icon={ethUrl} />}
+              <Pill text={authClient} start={12} end={10} icon={icUrl} />
               <CustomCopy size={"small"} copyText={authClient} />
               <RefreshIcon
                 className={`h-4 w-4 cursor-pointer fill-PrimaryTextColorLight dark:fill-PrimaryTextColor ${
@@ -70,9 +77,9 @@ const TopBarComponent = ({ isLoginPage }: { isLoginPage: boolean }) => {
             </div>
           )}
         </div>
-        <div className="flex flex-row justify-start items-center pr-9 gap-9">
+        <div className="flex flex-row items-center justify-start pr-9 gap-9">
           {!isLoginPage && (
-            <div className="flex flex-row justify-start items-center gap-2 text-md">
+            <div className="flex flex-row items-center justify-start gap-2 text-md">
               <WalletIcon className="fill-SvgColor dark:fill-SvgColor max-w-[1.5rem] h-auto"></WalletIcon>
               <p className="opacity-70">{t("total.balance")}:</p>
               <p className="font-medium">{`$${getTotalAmountInCurrency().toFixed(2)}`}</p>
