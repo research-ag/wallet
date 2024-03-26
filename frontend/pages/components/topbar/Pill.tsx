82408601ca6170a5bfbae9b3952a5da3da66809c
JairSDS <jair@sdsharp.com>
2024-03-26 11:28:56 -0500

    fix short principal in pill

--- frontend/pages/components/topbar/Pill.tsx
+++ frontend/pages/components/topbar/Pill.tsx
@@ -1,9 +1,11 @@
+import { shortAddress } from "@/utils";
+
 export default function Pill({ text, start, end, icon }: { text: string; start: number; end: number; icon?: string }) {
   return (
     <div className="px-3 py-1 rounded-full bg-GrayColor/50">
       <div className="flex items-center justify-center w-full gap-2 whitespace-nowrap">
         <img src={icon} alt="icon" className="w-5" />
-        {text?.slice(0, start) + "..." + text?.slice(-end)}
+        {shortAddress(text, start, end)}
       </div>
     </div>
   );
