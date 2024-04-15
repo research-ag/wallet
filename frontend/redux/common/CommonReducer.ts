fd92b4d5fb546b4d008c91e3f54b92cd423203b5
Julio DEV <julio.f@sdsharp.com>
2024-04-15 14:11:16 -0400

    feat: assets worker structure

--- frontend/redux/common/CommonReducer.ts
+++ frontend/redux/common/CommonReducer.ts
@@ -2,10 +2,13 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface CommonState {
   isAppDataFreshing: boolean;
+  lastDataRefresh: string;
+
 }
 
 const initialState: CommonState = {
   isAppDataFreshing: false,
+  lastDataRefresh: new Date().toISOString(),
 };
 
 const commonSlice = createSlice({
@@ -15,8 +18,11 @@ const commonSlice = createSlice({
     setAppDataRefreshing(state, action: PayloadAction<boolean>) {
       state.isAppDataFreshing = action.payload;
     },
+    setLastDataRefresh(state, action: PayloadAction<string>) {
+      state.lastDataRefresh = action.payload;
+    },
   },
 });
 
-export const { setAppDataRefreshing } = commonSlice.actions;
+export const { setAppDataRefreshing, setLastDataRefresh } = commonSlice.actions;
 export default commonSlice.reducer;
--- frontend/wrappers/WorkersWrapper.tsx
+++ frontend/wrappers/WorkersWrapper.tsx
@@ -1,10 +1,44 @@
-import { useEffect } from "react";
+import { setAppDataRefreshing, setLastDataRefresh } from "@redux/common/CommonReducer";
+import { useAppDispatch, useAppSelector } from "@redux/Store";
+import { useEffect, useMemo } from "react";
+
+
+const WORKER_INTERVAL = 10 * 1000; // 10 seconds
 
 export default function WorkersWrapper({ children }: { children: React.ReactNode }) {
+  const dispatch = useAppDispatch();
+  const { lastDataRefresh, isAppDataFreshing } = useAppSelector((state) => state.common);
+
+  const assetWorker = useMemo(() => new Worker(new URL("../pages/workers/assets.ts", import.meta.url)), []);
 
   useEffect(() => {
-    console.log("worker running");
-  }, []);
+    if (window.Worker) {
+
+      const intervalId = setInterval(() => {
+        console.log("running interval");
+
+        const isOlderThanInterval =
+          new Date(lastDataRefresh).getTime() + WORKER_INTERVAL <
+          new Date().getTime();
+
+        if (!isAppDataFreshing && isOlderThanInterval) {
+          console.log("Running asset worker");
+          dispatch(setAppDataRefreshing(true));
+          assetWorker.postMessage("Good Job!: " + new Date().toISOString());
+        };
+      }, WORKER_INTERVAL);
+
+      assetWorker.onmessage = (event: MessageEvent<string>) => {
+        console.log("worker message received", event);
+        dispatch(setLastDataRefresh(event.data));
+        dispatch(setAppDataRefreshing(false));
+      };
+
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+  }, [assetWorker, dispatch, isAppDataFreshing, lastDataRefresh]);
 
   return <>{children}</>;
 };
