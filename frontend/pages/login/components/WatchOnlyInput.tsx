308221383e93810bc60c111c7c4659a43aa3c62f
Julio DEV <julio.f@sdsharp.com>
2024-04-22 06:27:33 -0400

    feat: resize historical options list

--- frontend/pages/login/components/WatchOnlyInput.tsx
+++ frontend/pages/login/components/WatchOnlyInput.tsx
@@ -6,6 +6,7 @@ import { handlePrincipalAuthenticated } from "@redux/CheckAuth";
 import { clsx } from "clsx";
 import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
 
+
 interface WatchOnlyInputProps {
   principalAddress: string;
   setPrincipalAddress: Dispatch<SetStateAction<string>>;
@@ -15,21 +16,40 @@ export default function WatchOnlyInput(props: WatchOnlyInputProps) {
   const { principalAddress, setPrincipalAddress } = props;
   const [watchOnlyLoginErr, setWatchOnlyLoginErr] = useState(false);
 
+  const [historicalOpen, setHistoricalOpen] = useState(false);
+  const [historicalFocus, setHistoricalFocus] = useState(false);
+
+  function onOpenChangeHandler(open: boolean) {
+    console.log('Popover open:', open);
+  };
+
+  function onFofucChangeHandler(focus: boolean) {
+    console.log('Focus:', focus);
+  };
+
   return (
-    <CustomInput
-      sizeInput={"medium"}
-      intent={"secondary"}
-      compOutClass=""
-      value={principalAddress}
-      onChange={onPrincipalChange}
-      border={watchOnlyLoginErr ? "error" : undefined}
-      // eslint-disable-next-line jsx-a11y/no-autofocus
-      autoFocus
-      sufix={<CheckIcon className={getCheckIconStyles(principalAddress, watchOnlyLoginErr)} />}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") handlePrincipalAuthenticated(principalAddress);
-      }}
-    />
+    <div className="relative w-full">
+      <CustomInput
+        sizeInput={"medium"}
+        intent={"secondary"}
+        compOutClass=""
+        value={principalAddress}
+        onChange={onPrincipalChange}
+        border={watchOnlyLoginErr ? "error" : undefined}
+        onFocus={() => console.log("focus")}
+        // eslint-disable-next-line jsx-a11y/no-autofocus
+        autoFocus
+        sufix={<CheckIcon className={getCheckIconStyles(principalAddress, watchOnlyLoginErr)} />}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") handlePrincipalAuthenticated(principalAddress);
+        }}
+      />
+      <div className="absolute z-10 w-full mt-1 bg-red-300">
+        {Array.from({ length: 5 }).map((_, i) => (
+          <p key={i}>Historical options</p>
+        ))}
+      </div>
+    </div>
   );
 
   function onPrincipalChange(e: ChangeEvent<HTMLInputElement>) {
