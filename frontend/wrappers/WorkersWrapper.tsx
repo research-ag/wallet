085c8908d57c819e9b5b70c7b4205a521f309ec8
Julio DEV <julio.f@sdsharp.com>
2024-05-09 08:45:02 -0400

    feat: intial load flow

--- frontend/wrappers/WorkersWrapper.tsx
+++ frontend/wrappers/WorkersWrapper.tsx
@@ -1,24 +1,23 @@
 import { useEffect, useRef } from "react";
 import { useAppDispatch, useAppSelector } from "@redux/Store";
 import { setAppDataRefreshing } from "@redux/common/CommonReducer";
-// import { AssetSymbolEnum } from "@/common/const";
-// import contactCacheRefresh from "@pages/contacts/helpers/contactCacheRefresh";
-// import { allowanceCacheRefresh } from "@pages/allowances/helpers/cache";
-// import { updateAllBalances } from "@redux/assets/AssetActions";
-// import { setAppDataRefreshing, setLastDataRefresh } from "@redux/common/CommonReducer";
-// import { Asset } from "@redux/models/AccountModels";
-// import { setTxWorker } from "@redux/transaction/TransactionReducer";
-// import dayjs from "dayjs";
-// import { db } from "@/database/db";
-// import { hexToUint8Array } from "@common/utils/hexadecimal";
-// import { getAllTransactionsICP, getAllTransactionsICRC1 } from "@pages/home/helpers/requests";
+import { db } from "@/database/db";
+import { updateAllBalances } from "@redux/assets/AssetActions";
+import { AssetSymbolEnum } from "@common/const";
+import { Asset } from "@redux/models/AccountModels";
+import { getAllTransactionsICP, getAllTransactionsICRC1 } from "@pages/home/helpers/requests";
+import { setTxWorker } from "@redux/transaction/TransactionReducer";
+import { hexToUint8Array } from "@common/utils/hexadecimal";
+import { allowanceCacheRefresh } from "@pages/allowances/helpers/cache";
+import contactCacheRefresh from "@pages/contacts/helpers/contactCacheRefresh";
 
 const WORKER_INTERVAL = 10 * 60 * 1000; // 10 minutes
 
 export default function WorkersWrapper({ children }: { children: React.ReactNode }) {
   const { isAppDataFreshing } = useAppSelector((state) => state.common);
+  const { userAgent } = useAppSelector((state) => state.auth);
   const dispatch = useAppDispatch();
-  // const { assets } = useAppSelector((state) => state.asset.list);
+  const { assets } = useAppSelector((state) => state.asset.list);
   // const { transactions } = useAppSelector((state) => state.transaction.list);
   // const { contacts } = useAppSelector((state) => state.contacts);
   // const { allowances } = useAppSelector((state) => state.allowance.list);
@@ -26,132 +25,95 @@ export default function WorkersWrapper({ children }: { children: React.ReactNode
 
   // const dispatch = useAppDispatch();
 
-  // async function fetchICPTransactions(asset: Asset) {
-  //   for (const subAccount of asset.subAccounts) {
-  //     const transactions = await getAllTransactionsICP({
-  //       subaccount_index: subAccount.sub_account_id,
-  //       isOGY: asset.tokenSymbol === AssetSymbolEnum.Enum.OGY,
-  //     });
+  async function fetchICPTransactions(asset: Asset) {
+    // TODO: TODO: set all transactions inside the redux to reduce the renders
+    for (const subAccount of asset.subAccounts) {
+      const transactions = await getAllTransactionsICP({
+        subaccount_index: subAccount.sub_account_id,
+        isOGY: asset.tokenSymbol === AssetSymbolEnum.Enum.OGY,
+      });
 
-  //     // TODO: running worker
-  //     dispatch(
-  //       setTxWorker({
-  //         tx: transactions,
-  //         symbol: asset.symbol,
-  //         tokenSymbol: asset.tokenSymbol,
-  //         subaccount: subAccount.sub_account_id,
-  //       }),
-  //     );
-  //   }
-  // }
+      dispatch(
+        setTxWorker({
+          tx: transactions,
+          symbol: asset.symbol,
+          tokenSymbol: asset.tokenSymbol,
+          subaccount: subAccount.sub_account_id,
+        }),
+      );
+    }
+  }
 
-  // async function fetchICRC1Transactions(asset: Asset, selectedToken: Asset) {
-  //   const workerTransactions = [];
+  async function fetchICRC1Transactions(asset: Asset, selectedToken: Asset) {
+    // TODO: TODO: set all transactions inside the redux to reduce the renders
+    for (const subAccount of asset.subAccounts) {
+      const transactions = await getAllTransactionsICRC1({
+        canisterId: selectedToken.index || "",
+        subaccount_index: hexToUint8Array(subAccount.sub_account_id || "0x0"),
+        assetSymbol: asset.tokenSymbol,
+        canister: selectedToken.address,
+        subNumber: subAccount.sub_account_id,
+      });
 
-  //   for (const subAccount of asset.subAccounts) {
+      dispatch(
+        setTxWorker({
+          tx: transactions,
+          symbol: asset.symbol,
+          tokenSymbol: asset.tokenSymbol,
+          subaccount: subAccount.sub_account_id,
+        }),
+      );
+    }
+  }
 
-  //     const transactions = await getAllTransactionsICRC1({
-  //       canisterId: selectedToken.index || "",
-  //       subaccount_index: hexToUint8Array(subAccount.sub_account_id || "0x0"),
-  //       assetSymbol: asset.tokenSymbol,
-  //       canister: selectedToken.address,
-  //       subNumber: subAccount.sub_account_id,
-  //     });
+  async function transactionCacheRefresh() {
+    try {
+      for (const asset of assets) {
+        console.log(asset.name);
 
-  //     workerTransactions.push({
-  //       tx: transactions,
-  //       symbol: asset.symbol,
-  //       tokenSymbol: asset.tokenSymbol,
-  //       subaccount: subAccount.sub_account_id,
-  //     })
-
-  //   }
-  //   console.log("END", workerTransactions);
-
-  // }
-
-  // async function transactionCacheRefresh() {
-  //   try {
-  //     const assetsDB = await db().getAssets();
-
-  // for (const asset of assetsDB) {
-
-  //   console.log(asset.name);
-
-  // if (asset.tokenSymbol === AssetSymbolEnum.Enum.ICP || asset.tokenSymbol === AssetSymbolEnum.Enum.OGY) {
-  //   await fetchICPTransactions(asset);
-  // } else {
-  //   const selectedAsset = assets.find((currentAsset) => currentAsset.symbol === asset.symbol);
-  //   if (selectedAsset) {
-  //     await fetchICRC1Transactions(asset, selectedAsset);
-  //   }
-  // }
-
-  // }
-  //   } catch (error) {
-  //     console.error("Error in transactionCacheRefresh worker", error);
-  //   }
-  // }
-
-  // async function dataRefresh() {
-  //   try {
-  //     console.log("information");
-
-  // const assetsDB = await db().getAssets();
-  // await updateAllBalances({
-  //   loading: true,
-  //   myAgent: userAgent,
-  //   assets: assetsDB.length !== 0 ? assetsDB : [],
-  //   basicSearch: false,
-  // });
-  // await transactionCacheRefresh();
-  // await allowanceCacheRefresh();
-  // await contactCacheRefresh();
-  //   } catch (error) {
-  //     console.error("Error in dataRefresh worker", error);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   const timer = setInterval(() => {
-
-  //     if (!isAppDataFreshing) {
-  //       dispatch(setAppDataRefreshing(true));
-
-  //       dataRefresh().then(() => {
-  //         dispatch(setLastDataRefresh(dayjs().toISOString()));
-  //         dispatch(setAppDataRefreshing(false));
-  //       });
-  //     }
-  //   }, WORKER_INTERVAL);
-
-  //   return () => {
-  //     clearInterval(timer);
-  //   };
-  // }, [lastDataRefresh, isAppDataFreshing]);
+        if (asset.tokenSymbol === AssetSymbolEnum.Enum.ICP || asset.tokenSymbol === AssetSymbolEnum.Enum.OGY) {
+          await fetchICPTransactions(asset);
+        } else {
+          const selectedAsset = assets.find((currentAsset) => currentAsset.symbol === asset.symbol);
+          if (selectedAsset) {
+            await fetchICRC1Transactions(asset, selectedAsset);
+          }
+        }
+      }
+    } catch (error) {
+      console.error("Error in transactionCacheRefresh worker", error);
+    }
+  }
 
   async function assetsRefreshManager() {
-    setTimeout(() => {
-      console.log("fetch and dispatch assets");
-    }, 2000);
+    console.log("fetch and dispatch assets STARTED");
+
+    const assetsDB = await db().getAssets();
+    await updateAllBalances({
+      loading: true,
+      myAgent: userAgent,
+      assets: assetsDB.length !== 0 ? assetsDB : [],
+      basicSearch: false,
+    });
+    console.log("fetch and dispatch assets FINISHED");
   }
 
   async function transactionsRefreshManager() {
-    setTimeout(() => {
-      console.log("fetch and dispatch transactions");
-    }, 2000);
+    console.log("fetch and dispatch transactions STARTED");
+    await transactionCacheRefresh();
+    console.log("fetch and dispatch transactions END");
   }
 
   async function allowancesRefreshManager() {
-    setTimeout(() => {
-      console.log("fetch and dispatch allowances");
-    }, 2000);
+    console.log("fetch and dispatch allowances STARTED");
+    await allowanceCacheRefresh();
+    console.log("fetch and dispatch allowances END");
   }
 
   async function contactsRefreshManager() {
-    setTimeout(() => {
-      console.log("fetch and dispatch contacts");
-    }, 2000);
+    console.log("fetch and dispatch contacts STARTED");
+    await contactCacheRefresh();
+    console.log("fetch and dispatch contacts END");
   }
 
   useEffect(() => {
