fb172adf34612df0e869bea45af3995d3f654451
Julio DEV <julio.f@sdsharp.com>
2024-05-14 18:48:22 -0400

    sort allowance amounts

--- frontend/pages/allowances/helpers/sorters.ts
+++ frontend/pages/allowances/helpers/sorters.ts
@@ -1,6 +1,7 @@
 import { TAllowance } from "@/@types/allowance";
 import { SortOrder, SortOrderEnum } from "@/@types/common";
 import { excludeNamesFromAllowance, includeNamesToAllowances } from "./mappers";
+import { toHoleBigInt } from "@common/utils/amount";
 
 export function sortBySubAccount(order: SortOrder, filteredData: TAllowance[]) {
   const includedNames = includeNamesToAllowances(filteredData);
@@ -39,10 +40,31 @@ export function sortBySpender(order: SortOrder, filteredData: TAllowance[]): TAl
   return [...noSpenderNamed, ...excludeNamesFromAllowance(sorted)];
 }
 
+function fixDecimals(amount: string) {
+  const regex = /^[\+-]?\d*(?:\.\d{1,8})?$/;
+
+  if (regex.test(amount)) {
+    return amount;
+  } else {
+    const [integerPart, decimalPart] = amount.split(".");
+    const truncatedDecimal = decimalPart ? decimalPart.slice(0, 8) : "";
+    return `${integerPart}.${truncatedDecimal}`;
+  }
+}
+
 export function sortByAmount(order: SortOrder, filteredData: TAllowance[]): TAllowance[] {
+  // INFO: 8 is the most accuracy, that's why it's fixed number. Remove this comment if this behaviors changes.
   return order === SortOrderEnum.Values.ASC
-    ? [...filteredData].sort((a, b) => parseFloat(a.amount || "0") - parseFloat(b.amount || "0"))
-    : [...filteredData].sort((a, b) => parseFloat(b.amount || "0") - parseFloat(a.amount || "0"));
+    ? [...filteredData].sort((a, b) => {
+      const aAmount = toHoleBigInt(fixDecimals(a.amount || "0"), 8);
+      const bAmount = toHoleBigInt(fixDecimals(b.amount || "0"), 8);
+      return Number(aAmount - bAmount);
+    })
+    : [...filteredData].sort((a, b) => {
+      const aAmount = toHoleBigInt(fixDecimals(a.amount || "0"), 8);
+      const bAmount = toHoleBigInt(fixDecimals(b.amount || "0"), 8);
+      return Number(bAmount - aAmount);
+    });
 }
 
 export function sortByExpiration(order: SortOrder, filteredData: TAllowance[]): TAllowance[] {
