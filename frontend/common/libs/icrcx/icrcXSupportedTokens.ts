3c30ace281a696301f5cd56727b52d667276c98b
Julio DEV <julio.f@sdsharp.com>
2024-05-16 15:09:46 -0400

    icrcx icrcXSupportedTokens

---
+++ frontend/common/libs/icrcx/icrcXSupportedTokens.ts
@@ -0,0 +1,19 @@
+import { Principal } from "@dfinity/principal";
+import { ICRCXActor } from "@/common/libs/icrcx/actor";
+import { HttpAgent } from "@dfinity/agent";
+
+interface IcrcXSupportedTokensArgs {
+  address: string | Principal;
+  agent: HttpAgent;
+};
+
+export default async function icrcXSupportedTokens(args: IcrcXSupportedTokensArgs) {
+  const { address, agent } = args;
+
+  const actor = ICRCXActor({
+    address,
+    agent,
+  });
+
+  return await actor.icrcXSupportedTokens();
+};
