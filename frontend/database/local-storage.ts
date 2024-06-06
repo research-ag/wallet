a03e8919073c22012484b095388f88c38379b9c6
Julio DEV <julio.f@sdsharp.com>
2024-06-06 12:47:43 -0400

    fix: change selected asset on add contact

--- frontend/database/local-storage.ts
+++ frontend/database/local-storage.ts
@@ -176,7 +176,6 @@ export class LocalStorageDatabase extends IWalletDatabase {
     const contacts = this._getContacts();
     const databaseContact = this._getStorableContact(contact);
     this._setContacts([...contacts, databaseContact]);
-    console.log({ contacts, databaseContact });
     if (options?.sync) store.dispatch(addReduxContact(contact));
   }
 
