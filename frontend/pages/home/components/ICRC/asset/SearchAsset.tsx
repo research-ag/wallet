9064e19d4e5f7e0f6abce5a2d0572b14e30bcf11
Julio DEV <julio.f@sdsharp.com>
2024-06-11 08:04:37 -0400

    fix: asset search

--- frontend/pages/home/components/ICRC/asset/SearchAsset.tsx
+++ frontend/pages/home/components/ICRC/asset/SearchAsset.tsx
@@ -12,40 +12,52 @@ interface SearchAssetProps {
 export default function SearchAsset(props: SearchAssetProps) {
   const { searchKey, setSearchKey } = props;
   const dispatch = useAppDispatch();
-  const { selectedAsset, accordionIndex } = useAppSelector((state) => state.asset.helper);
   const { assets } = useAppSelector((state) => state.asset.list);
   const { t } = useTranslation();
 
   useEffect(() => {
-    const auxAssets = assets.filter((asset) => {
-      let includeInSub = false;
-      asset.subAccounts?.map((sa) => {
-        if (sa.name.toLowerCase().includes(searchKey.toLowerCase())) includeInSub = true;
-      });
-
-      return asset.name?.toLowerCase().includes(searchKey.toLowerCase()) || includeInSub || searchKey === "";
-    });
-
-    if (auxAssets.length > 0) {
-      const auxAccordion: string[] = [];
-
-      auxAssets.map((ast) => {
-        if (accordionIndex.includes(ast.tokenSymbol)) auxAccordion.push(ast.tokenSymbol);
-      });
-
-      dispatch(setAccordionAssetIdx(auxAccordion));
-
-      const isSelected = auxAssets.find((ast) => ast.tokenSymbol === selectedAsset?.tokenSymbol);
-      if (selectedAsset && !isSelected) {
-        dispatch(setSelectedAsset(auxAssets[0]));
-        auxAssets[0].subAccounts.length > 0 && dispatch(setSelectedAccount(auxAssets[0].subAccounts[0]));
+    // INFO: auto select asset, account and set accordion index on search
+    if (searchKey === "") {
+      if (assets.length > 0) {
+        dispatch(setSelectedAsset(assets[0]));
+        assets[0].subAccounts.length > 0 && dispatch(setSelectedAccount(assets[0].subAccounts[0]));
+        dispatch(setAccordionAssetIdx([assets[0].tokenSymbol]));
       }
-    } else {
-      dispatch(setAccordionAssetIdx([]));
-    }
-  }, [searchKey]);
+    };
 
-  // TODO: add debounce to setSearchKey
+    if (searchKey.trim().length > 0) {
+      const searchKeyLower = searchKey.toLowerCase();
+      const searchKeyFilteredAssets = assets.filter((asset) => {
+        const isAssetNameIncluded = asset.name?.toLowerCase().includes(searchKeyLower);
+        const isAssetSymbolIncluded = asset.symbol?.toLowerCase().includes(searchKeyLower);
+
+        const isSubAccountIncluded = asset.subAccounts
+          .map((subAccount) => {
+            const isSubAccountNameIncluded = subAccount.name.toLowerCase().includes(searchKeyLower);
+            const isSubAccountIdIncluded = subAccount.sub_account_id.toLowerCase().includes(searchKeyLower);
+            return isSubAccountNameIncluded || isSubAccountIdIncluded;
+          })
+          .includes(true);
+
+        return isAssetNameIncluded || isAssetSymbolIncluded || isSubAccountIncluded;
+      });
+
+      if (searchKeyFilteredAssets.length > 0) {
+        const selectedAsset = searchKeyFilteredAssets[0];
+        dispatch(setSelectedAsset(selectedAsset));
+        selectedAsset.subAccounts.length > 0 && dispatch(setSelectedAccount(selectedAsset.subAccounts[0]));
+
+        const accordionIndex = searchKeyFilteredAssets.map((asset) => asset.tokenSymbol);
+        dispatch(setAccordionAssetIdx(accordionIndex));
+      } else {
+        if (assets.length > 0) {
+          dispatch(setSelectedAsset(assets[0]));
+          assets[0].subAccounts.length > 0 && dispatch(setSelectedAccount(assets[0].subAccounts[0]));
+          dispatch(setAccordionAssetIdx([assets[0].tokenSymbol]));
+        }
+      }
+    };
+  }, [searchKey]);
 
   return (
     <input
