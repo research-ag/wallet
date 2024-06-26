969a9d06095125f4da0d6f4c71f4a4550b5cbc01
Julio DEV <julio.f@sdsharp.com>
2024-06-26 11:04:51 -0400

    fix: pill length

--- frontend/pages/components/topbar/Pill.tsx
+++ frontend/pages/components/topbar/Pill.tsx
@@ -13,7 +13,7 @@ export default function Pill({ text, start, end, icon }: PillProps) {
       <div className="px-3 py-1 rounded-full bg-GrayColor/50">
         <div className="flex items-center justify-center w-full gap-2 whitespace-nowrap">
           <img src={icon} alt="icon" className="w-5" />
-          {shortAddress(text, start, end)}
+          {text.length > 20 ? shortAddress(text, start, end) : text}
         </div>
       </div>
     </div>
